471809fdb17034531d43cfedc747090ce5f96fef
Boaz James <boaz.otieno@little.africa>
2024-06-14 10:26:39 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftDocumentIntelligence",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftDocumentIntelligence",
+            targets: ["SwiftDocumentIntelligence"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftDocumentIntelligence"),
+        .testTarget(
+            name: "SwiftDocumentIntelligenceTests",
+            dependencies: ["SwiftDocumentIntelligence"]),
+    ]
+)
